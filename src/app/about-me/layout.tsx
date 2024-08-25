67818aa63c40d7343fee83c570de63a959c6afb2
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2024-08-25 21:00:02 +0330

    [WIP] sub menu in about

--- src/app/about-me/layout.tsx
+++ src/app/about-me/layout.tsx
@@ -18,19 +18,32 @@ export default function AboutMe({
     <div className='flex h-full flex-col md:flex-row'>
       <div id='top-section' className='flex flex-col gap-0.5 border-e border-line p-2 text-tSecondary md:h-full md:p-4'>
         {util.map((item) => (
-          <Link
-            key={item.path}
-            href={item.path}
-            className={`flex shrink-0 items-center text-nowrap text-sm duration-300 ${pathname === item.path ? 'rounded-md bg-accent-purple/10 py-0.5 font-[500] text-tPrimary' : 'font-[400]'}`}
-          >
-            <RiArrowRightSLine className={clsx('duration-500', pathname === item.path ? 'rotate-90' : '')} />
-            {pathname !== item.path ? (
-              <RiFolder3Fill className='me-1 text-accent-orange' size={'1.25rem'} />
-            ) : (
-              <RiFolderOpenFill className='me-1' size={'1.25rem'} />
-            )}
-            <span className='self-end'>{item.label}</span>
-          </Link>
+          <div className='flex flex-col' key={item.path}>
+            <Link
+              href={item.path}
+              className={`flex shrink-0 items-center text-nowrap text-sm duration-300 ${pathname === item.path ? 'rounded-md bg-accent-purple/10 py-0.5 font-[500] text-tPrimary' : 'font-[400]'}`}
+            >
+              <RiArrowRightSLine className={clsx('duration-500', pathname === item.path ? 'rotate-90' : '')} />
+              {pathname !== item.path ? (
+                <RiFolder3Fill className='me-1 text-accent-orange' size={'1.25rem'} />
+              ) : (
+                <RiFolderOpenFill className='me-1' size={'1.25rem'} />
+              )}
+              <span className='self-end pe-2'>{item.label}</span>
+            </Link>
+            <div
+              className={clsx(
+                'flex flex-col gap-0.5 overflow-hidden transition-all duration-300',
+                pathname === item.path ? 'h-12' : 'pointer-events-none h-0'
+              )}
+            >
+              {item.sub.map((i) => (
+                <span className='ps-8 text-sm font-[300]' key={i}>
+                  {i}
+                </span>
+              ))}
+            </div>
+          </div>
         ))}
       </div>
       <div
@@ -45,10 +58,14 @@ export default function AboutMe({
 }
 
 const util = [
-  { path: '/about-me/summery', label: 'Summery' },
-  { path: '/about-me/experience', label: 'Experience' },
-  { path: '/about-me/education', label: 'Education' },
-  { path: '/about-me/certifications', label: 'Certifications' },
-  { path: '/about-me/technical-skills', label: 'Technical Skills' },
-  { path: '/about-me/languages', label: 'Languages' },
+  { path: '/about-me/summery', label: 'Summery', sub: ['index'] },
+  { path: '/about-me/experience', label: 'Experience', sub: ['IroTeam', 'Wallex Exchange'] },
+  { path: '/about-me/education', label: 'Education', sub: ['Bachelor', 'Diploma'] },
+  { path: '/about-me/certifications', label: 'Certifications', sub: ['index'] },
+  {
+    path: '/about-me/technical-skills',
+    label: 'Technical Skills',
+    sub: ['Frontend Core', 'Frameworks and Libraries', 'Styling and UI', 'Tools and version control'],
+  },
+  { path: '/about-me/languages', label: 'Languages', sub: ['Persian', 'English'] },
 ];
