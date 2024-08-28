bf2c54482377afc4a57d323624aa271e2c0b938c
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2024-08-28 18:04:09 +0330

    [WIP-MGH] contact page code section

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Button } from '@nextui-org/button';
 import { Input, Textarea } from '@nextui-org/input';
+
+import { DateTime } from 'luxon';
 
 export default function ContactMe() {
   //STATES
@@ -12,11 +14,27 @@ export default function ContactMe() {
     email: '',
     message: '',
   });
+
+  const [lineNumber, setLineNumber] = useState(12);
+
+  // REF
+  const textRef = useRef<HTMLDivElement>(null);
+
+  //LIFE CYCLE HOOKS
+  useEffect(() => {
+    const handleResize = () => {
+      setLineNumber((textRef.current?.offsetHeight ?? 0) / 24);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [textRef, textRef?.current?.offsetHeight]);
+
   return (
     <div className='flex h-full'>
-      <div className='w-[10%] border-e border-line'>a</div>
-      <div className='flex w-[45%] flex-col items-center justify-center gap-5 border-e border-line'>
-        <form className='flex w-[60%] flex-col gap-3'>
+      <div className='w-[279px] shrink-0 border-e border-line'>a</div>
+      <div className='flex w-full flex-col items-center justify-center gap-5 border-line xl:w-[40%] xl:border-e'>
+        <form className='flex w-[70%] flex-col gap-3'>
           <Input
             classNames={{
               label: '!text-tSecondary !font-[450]',
@@ -47,6 +65,7 @@ export default function ContactMe() {
             onChange={(e) => setFormData({ ...formData, email: e.target.value })}
           />
           <Textarea
+            minRows={12}
             classNames={{
               label: '!text-tSecondary !font-[450]',
               input: '!text-[#465E77]',
@@ -65,26 +84,68 @@ export default function ContactMe() {
           </Button>
         </form>
       </div>
-      <div className='flex w-[45%] items-center justify-center gap-6 p-5'>
-        <div className='flex flex-col items-end text-tSecondary'>
-          {[...new Array(11)].map((_, index) => (
+      <div className='hidden w-[60%] items-center justify-center gap-4 truncate text-tSecondary xl:flex'>
+        <div className='flex flex-col items-end'>
+          {[...new Array(lineNumber ? lineNumber : 12)].map((_, index) => (
             <span className='' key={index}>
               {index + 1}
             </span>
           ))}
         </div>
-        <div className='flex flex-col truncate whitespace-pre-line text-nowrap text-tPrimary'>
-          {`const button = document.querySelector('#submit');
-
-const message = {
-	name: "${formData.name}",
-	email: "${formData.email}",
-	message: "${formData.message}",
-}
-
-button.addEventListener('click', () => {
-	form.send(message);
-})`}
+        <div ref={textRef} className='flex max-w-[90%] flex-col truncate whitespace-pre-line text-nowrap'>
+          <div className='text-nowrap'>
+            <span className='text-accent-purple'>const </span>
+            <span className='text-accent-blue'>button</span>
+            <span className='text-accent-purple'> = </span>
+            <span className='text-accent-blue'>document.getElementById</span>(
+            <span className='text-accent-orange'>{'#submit'}</span>);
+          </div>
+          <br />
+          <div className='text-nowrap'>
+            <span className='text-accent-purple'>const </span>
+            <span className='text-accent-blue'>message</span>
+            <span className='text-accent-purple'> = </span> {`{`}
+          </div>
+          <div className='text-nowrap'>
+            <span className='text-accent-blue'>name: </span>
+            <span className='text-accent-orange'>{`"${formData.name}"`}</span>,
+          </div>
+          <div className='text-nowrap'>
+            <span className='text-accent-blue'>email: </span>
+            <span className='text-accent-orange'>{`"${formData.email}"`}</span>,
+          </div>
+          <div className='flex'>
+            <div>
+              <span className='text-accent-blue'>message: </span>
+              <span className='text-accent-orange'>{`"`}</span>
+            </div>
+            <span className='whitespace-pre-line text-accent-orange'>
+              {formData.message}
+              {`"`}
+              <span className='text-tSecondary'>,</span>
+            </span>
+          </div>
+          <div className='text-nowrap'>
+            <span className='text-accent-blue'>date: </span>
+            <span className='text-accent-orange'>{`"${DateTime.now().startOf('minute').toUTC()}"`}</span>,
+          </div>
+          <div>{`}`}</div>
+          <br />
+          <div className='text-nowrap'>
+            <span className='text-accent-blue'>button</span>.<span className='text-accent-blue'>addEventListener</span>
+            {`(`}
+            <span className='text-accent-orange'>{`'click`}</span>
+            {`, ()`}
+            <span className='text-accent-purple'>{` => `}</span> {'{'}
+          </div>
+          <div className='text-nowrap'>
+            {' '}
+            <span className='text-accent-blue'>form</span>.<span className='text-accent-blue'>send</span>
+            {`(`}
+            <span className='text-accent-blue'>message</span>
+            {`);`}
+          </div>
+          <div className='text-nowrap'>{`})`}</div>
         </div>
       </div>
     </div>
