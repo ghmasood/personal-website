f38cfdd9c09c30ddbba26cdec591949eb388551b
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2024-09-08 19:22:17 +0330

    [ADD] some plugin with configs to backend

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+export default ({ env }) => ({
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
+  "protected-populate": {
+    enabled: true,
+    config: {
+      ["auto-populate"]: true,
+    },
+  },
+});
