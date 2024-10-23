55145437e6177012dbc30732628d84edbcbb72ce
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2024-10-23 21:50:12 +0330

    [BUGFIX] storage

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -3,14 +3,16 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('LIARA_ACCESS_KEY_ID'),
-        secretAccessKey: env('LIARA_ACCESS_SECRET'),
-        region: env('LIARA_REGION'),
-        endpoint: env('LIARA_ENDPOINT'), // Add endpoint variable here
+        credentials: {
+          accessKeyId: env('ARVAN_ACCESS_KEY_ID'),
+          secretAccessKey: env('ARVAN_ACCESS_SECRET'),
+        },
+        region: env('ARVAN_REGION'),
+        endpoint: env('ARVAN_ENDPOINT'), // Add endpoint variable here
         params: {
           ACL: env('AWS_ACL', 'public-read'),
           signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('LIARA_BUCKET'),
+          Bucket: env('ARVAN_BUCKET'),
         },
       },
       actionOptions: {
@@ -27,12 +29,6 @@ export default ({ env }) => ({
       responseTransforms: {
         removeDataKey: true,
       },
-    },
-  },
-  'protected-populate': {
-    enabled: true,
-    config: {
-      ['auto-populate']: true,
     },
   },
 });
